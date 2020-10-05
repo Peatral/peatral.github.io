1fea43e0c359b09f8b1d8ab5778146c946483c8f
Christof Reimers <christof.reimers@outlook.de>
2020-10-05 10:55:26 +0200

    Remove Homebutton on PrintPdf + AutoPrint

--- presentations/pdfThemeSwap.js
+++ presentations/pdfThemeSwap.js
@@ -1,12 +1,17 @@
 if (window.location.search.match( /print-pdf/gi )) {
-    var theme = document.createElement( 'link' );
-    theme.rel = 'stylesheet';
-    theme.type = 'text/css';
-    theme.href =  'dist/theme/white.css';
-    document.getElementsByTagName( 'head' )[0].appendChild( theme );
-    var pdfcss = document.createElement( 'link' );
-    pdfcss.rel = 'stylesheet';
-    pdfcss.type = 'text/css';
-    pdfcss.href = 'print-pdf.css'
-    document.getElementsByTagName( 'head' )[0].appendChild( pdfcss );
+    var theme = document.createElement( "link" );
+    theme.rel = "stylesheet";
+    theme.type = "text/css";
+    theme.href =  "dist/theme/white.css";
+    document.getElementsByTagName( "head" )[0].appendChild( theme );
+    var pdfcss = document.createElement( "link" );
+    pdfcss.rel = "stylesheet";
+    pdfcss.type = "text/css";
+    pdfcss.href = "print-pdf.css";
+    document.getElementsByTagName("head")[0].appendChild( pdfcss );
 }
+
+window.onload = function(){
+    document.getElementById("homebutton").remove();
+    print();
+ };
