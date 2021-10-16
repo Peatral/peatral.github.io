dcaa3a270a9c01b378269758577fc35cf669a730
Christof Reimers <christof.reimers@outlook.de>
2021-10-16 17:26:28 +0200

    Change domain, Merge Old 3rd Party Presentations, make revealjs submodule

--- presentations/pdfThemeSwap.js
+++ presentations/pdfThemeSwap.js
@@ -1,19 +1,19 @@
-if (window.location.search.match( /print-pdf/gi )) {
-    var theme = document.createElement( "link" );
-    theme.rel = "stylesheet";
-    theme.type = "text/css";
-    theme.href =  "dist/theme/white.css";
-    document.getElementsByTagName( "head" )[0].appendChild( theme );
-    var pdfcss = document.createElement( "link" );
-    pdfcss.rel = "stylesheet";
-    pdfcss.type = "text/css";
-    pdfcss.href = "print-pdf.css";
-    document.getElementsByTagName("head")[0].appendChild( pdfcss );
+if (window.location.search.match(/print-pdf/gi)) {
+  var theme = document.createElement("link");
+  theme.rel = "stylesheet";
+  theme.type = "text/css";
+  theme.href = "/reveal.js/dist/theme/white.css";
+  document.getElementsByTagName("head")[0].appendChild(theme);
+  var pdfcss = document.createElement("link");
+  pdfcss.rel = "stylesheet";
+  pdfcss.type = "text/css";
+  pdfcss.href = "print-pdf.css";
+  document.getElementsByTagName("head")[0].appendChild(pdfcss);
 }
 
-window.onload = function(){
-    if (window.location.search.match( /print-pdf/gi )) {
-        document.getElementById("homebutton").remove();
-        print();
-    }
+window.onload = function () {
+  if (window.location.search.match(/print-pdf/gi)) {
+    document.getElementById("homebutton").remove();
+    print();
+  }
 };
